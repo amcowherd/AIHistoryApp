7a5adcd3bf2465046534e6c253f7493e5be86f49
amcowherd <a.m.cowherd@gmail.com>
2025-06-23 10:23:50 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,12 +5,12 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC0nix5oC_SixNALjIdolvrAhtIu-i2t_Q",
-  authDomain: "aihistory-516d6.firebaseapp.com",
-  projectId: "aihistory-516d6",
-  storageBucket: "aihistory-516d6.firebasestorage.app",
-  messagingSenderId: "574357748437",
-  appId: "1:574357748437:web:914f87e19d235af695005c"
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " "
 };
 
 const app = initializeApp(firebaseConfig);
